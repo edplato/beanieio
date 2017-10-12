f46ab6e71d80225e73586682d55591b911eec7db
Dominic Ma <dominic.c.ma@gmail.com>
2017-10-11 22:12:58 -0700

    Added form and text area for journal

--- public/src/components/journal/journal.jsx
+++ public/src/components/journal/journal.jsx
@@ -4,14 +4,20 @@ import axios from 'axios';
 export default class Journal extends Component {
   constructor(props) {
     super(props);
-
+    this.state = {
+      entry: '',
+    }
 
   }
 
   render() {
     return (
-
-      <h1>This is the journal component</h1>
+      <div>
+        <h1>This is the journal component</h1>
+        <form onChange={e => this.setState({entry: e.target.value})}>
+          <textarea placeholder="Enter a journal entry" />
+        </form>
+      </div>
     )
   }
 }
