7dea754cf392f4c5328fbad713429248dc2184dd
Dominic Ma <dominic.c.ma@gmail.com>
2017-10-13 00:42:34 -0700

    Linked text to submit

--- public/src/components/journal/journal.jsx
+++ public/src/components/journal/journal.jsx
@@ -8,15 +8,22 @@ export default class Journal extends Component {
     this.state = {
       entry: '',
     }
-
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
+
+
+  handleSubmit(e) {
+    e && e.preventDefault();
+    console.log(this.state.entry);
+  }
+
 
   render() {
     return (
       <div className="journal-container">
         <div className="journal-header"><span>Journal</span></div>
           <div className="journal-content">
-            <form onChange={e => this.setState({entry: e.target.value})}>
+            <form onChange={e => this.setState({entry: e.target.value})}  onSubmit={this.handleSubmit} >
               <textarea placeholder="Enter a journal entry" />
               <input type="submit" />
             </form>
