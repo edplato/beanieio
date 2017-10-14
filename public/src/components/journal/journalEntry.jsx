4d6f0a7cdc79023ae5a0ad1ff739f610a1b8fa32
Dominic Ma <dominic.c.ma@gmail.com>
2017-10-14 14:36:21 -0700

    Aligned arrow buttons and fixed responsiveness of containers

--- public/src/components/journal/journalEntry.jsx
+++ public/src/components/journal/journalEntry.jsx
@@ -32,20 +32,17 @@ export default class JournalEntry extends Component {
     return (
       <div className="journalEntry-container shadow">
         <div className="journalEntry-header">
-          <span className="journalEntry-header-left"><i className="mdi mdi-arrow-left-bold"></i></span>
-          <span className="journalEntry-header-center">New Entry</span>
+        <span className="journalEntry-header-left"><i className="mdi mdi-arrow-left-bold"></i></span>
+          <div className="journalEntry-header-center">New Entry</div>
           <span className="journalEntry-header-right"><i className="mdi mdi-arrow-right-bold"></i></span>
 
         </div>
-          <div className="journalEntry-content">
-            <form onChange={e => this.setState({journalEntry: e.target.value})}  onSubmit={this.handleSubmit} >
-              <textarea className="journalEntry-area" placeholder="How was your day?" />
-              <div className="journal-submit-section flex flex-center">
-                {this.state.isNewEntry ? (<button type="submit" className="btn journal-submit-btn shadow">Submit</button>) : (null) }
-              </div>
-            </form>
-          </div>
-
+          <form className="journalEntry-content" onChange={e => this.setState({journalEntry: e.target.value})}  onSubmit={this.handleSubmit} >
+            <textarea className="journalEntry-area" placeholder="How was your day?" />
+            <div className="journal-submit-section flex flex-center">
+              {this.state.isNewEntry ? (<button type="submit" className="btn journal-submit-btn shadow">Submit</button>) : (null) }
+            </div>
+          </form>
       </div>
     )
   }
